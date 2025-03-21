95515dbe07834105a1fdd4ee338e14f911c8d7d8
Cipher3003 <shiveshdhiman@gmail.com>
2025-03-21 13:10:52 +0530

    Map integrated

--- SIMscripts/run_sim.py
+++ SIMscripts/run_sim.py
@@ -1,6 +1,6 @@
 import traci
 
-sumo_cmd = ["sumo-gui", "-c", r"C:\Users\sai\OneDrive\Documents\TLO\Traffic-Optimizer\config\trf_sim.sumocfg"]
+sumo_cmd = ["sumo-gui", "-c", r"C:\Users\sai\OneDrive\Documents\TLO\Traffic-Optimizer\ggn\osm.sumocfg"]
 traci.start(sumo_cmd)
 
 for step in range(1000):  # Run for 1000 time steps
